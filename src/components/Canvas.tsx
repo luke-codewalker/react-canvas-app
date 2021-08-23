c09bf34f8ac7342538562e477f520610c472aa13
Luke Codewalker <arathorn089@arcor.de>
2021-08-23 17:02:19 +0200

    basic animation loop done

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,10 +9,21 @@ export const Canvas = () => {
         const canvas = canvasRef.current;
         // getContext can return null for unknown contexts but with '2d' we can be sure it will work, hence the !
         const context = canvas.getContext('2d')!;
+        let i = 1;
 
-        context.beginPath();
-        context.arc(50, 50, 50, 0, 2 * Math.PI);
-        context.fill();
+        const draw = (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D): number => {
+            context.clearRect(0, 0, canvas.width, canvas.height);
+            context.beginPath();
+            context.arc(canvas.width / 2, canvas.height / 2, 50 * Math.abs(Math.cos(i)), 0, 2 * Math.PI);
+            context.fill();
+            i += 0.01;
+
+            return requestAnimationFrame(() => draw(canvas, context))
+        }
+
+        const animationId = draw(canvas, context);
+
+        return () => cancelAnimationFrame(animationId);
     })
 
     return <canvas ref={canvasRef}></canvas>
