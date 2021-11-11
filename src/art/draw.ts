21c5b723f2d01df00101b0292efccc49a11ebcec
Luke Codewalker <arathorn089@arcor.de>
2021-11-11 21:57:09 +0100

    add better explanation and exampel in docs

--- src/art/draw.ts
+++ src/art/draw.ts
@@ -2,11 +2,10 @@ import { Animation } from "../lib/canvas";
 const size = 20;
 
 export const draw: Animation<[number]> = (canvas: HTMLCanvasElement, context: CanvasRenderingContext2D, [stepSize]) => {
-    // this code is run before the animation whenever the external dependencies change
+    // this code is run before the animation whenever the external dependencies change or the animation is reset
     let x = 0;
     const y = canvas.height / 2;
     context.fillStyle = '#333';
-    let pausePassed = false;
 
     // this function is called to paint each frame of the animation
     return ({ frameCount, stopAnimation, startAnimation, resetAnimation }) => {
@@ -15,13 +14,15 @@ export const draw: Animation<[number]> = (canvas: HTMLCanvasElement, context: Ca
         context.fillRect(x, y, size, size);
         x = (x + stepSize) % canvas.width;
 
-        if (frameCount > 314 && !pausePassed) {
-            pausePassed = true;
+        if (frameCount === 314) {
+            // you can stop an (re)start the animation any time you like
             stopAnimation();
             setTimeout(startAnimation, 1000)
         };
 
         if (frameCount === canvas.width) {
+            // if you want to start over use resetAnimation(), this will call your draw function again
+            // and start y new animation loop
             resetAnimation();
         }
     }
